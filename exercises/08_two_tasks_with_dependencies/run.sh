86d0aafb568c7867d5297c6af8699afa284a81bd
Mark Veltzer <mark.veltzer@gmail.com>
2021-03-03 08:35:26 +0200

    all but deps

---
+++ exercises/08_two_tasks_with_dependencies/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+gradle :count_lines :check_no_regexp
---
+++ exercises/08_two_tasks_with_dependencies/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,9 @@
+package hello;
+
+/* Written by Dark */
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
